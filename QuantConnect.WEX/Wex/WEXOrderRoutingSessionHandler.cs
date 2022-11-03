d98968557117df49a794543b2d0a6082691146e5
sodamnfoolish <n.zhyikov@gmail.com>
2022-11-03 15:52:08 +0200

    feature: implemented WEXOrderRoutingSessionHandler.UpdateOrder()

--- QuantConnect.WEX/Wex/WEXOrderRoutingSessionHandler.cs
+++ QuantConnect.WEX/Wex/WEXOrderRoutingSessionHandler.cs
@@ -1,8 +1,12 @@
-﻿using QuantConnect.Orders;
+﻿using QuantConnect.Logging;
+using QuantConnect.Orders;
 using QuantConnect.WEX.Fix.Core;
 using QuantConnect.WEX.Fix.Protocol;
+using QuickFix;
 using QuickFix.Fields;
 using QuickFix.FIX42;
+using System.Net.Http.Headers;
+using System.Windows.Markup;
 
 namespace QuantConnect.WEX.Wex
 {
@@ -49,7 +53,57 @@ namespace QuantConnect.WEX.Wex
 
         public bool UpdateOrder(Order order)
         {
-            throw new NotImplementedException();
+            var request = new OrderCancelReplaceRequest
+            {
+                ClOrdID = new ClOrdID(WEXOrderId.GetNext()),
+                OrigClOrdID = new OrigClOrdID(order.BrokerId[0]),
+                HandlInst = new HandlInst(HandlInst.AUTOMATED_EXECUTION_ORDER_PRIVATE_NO_BROKER_INTERVENTION),
+                Symbol = new QuickFix.Fields.Symbol(order.Symbol.Value),     
+                TransactTime = new TransactTime(order.Time),
+                OrderQty = new OrderQty(order.Quantity)
+            };
+
+            switch (order.Direction)
+            {
+                case OrderDirection.Buy:
+                    request.Side = new Side(Side.BUY);
+                    break;
+
+                case OrderDirection.Sell:
+                    request.Side = new Side(Side.SELL);
+                    break;
+
+                default:
+                    Logging.Log.Error($"WEXOrderRoutingSessionHandler.UpdateOrder(): Not supported order direction - {order.Type}");
+                    break;
+            }
+
+            switch (order.Type)
+            {
+                case OrderType.Market:
+                    request.OrdType = new OrdType(OrdType.MARKET);
+                    break;
+
+                case OrderType.Limit:
+                    request.OrdType = new OrdType(OrdType.LIMIT);
+                    request.Price = new Price(order.Price);
+                    break;
+
+                case OrderType.StopLimit:
+                    request.OrdType = new OrdType(OrdType.STOP_LIMIT);
+                    request.Price = new Price(order.Price);
+                    break;
+
+                case OrderType.MarketOnClose:
+                    request.OrdType = new OrdType(OrdType.MARKET_ON_CLOSE);
+                    break;
+
+                default:
+                    Logging.Log.Error($"WEXOrderRoutingSessionHandler.UpdateOrder(): Not supported order type - {order.Type}");
+                    break;
+            }
+
+            return _session.Send(request);
         }
     }
 }
